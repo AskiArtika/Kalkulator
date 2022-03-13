d27b50af6a341f4f2cc412450a5adf55a4d744da
AskiArtika <100743439+AskiArtika@users.noreply.github.com>
2022-03-14 01:33:03 +0700

    Add files via upload

---
+++ Kalkulator.vb
@@ -0,0 +1,50 @@
+﻿Public Class Kalkulator
+    Dim x, y, r As Integer
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        x = Val(TextBox1.Text)
+        y = Val(TextBox2.Text)
+        r = x - y
+        TextBox3.Text = r
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        x = Val(TextBox1.Text)
+        y = Val(TextBox2.Text)
+        r = x * y
+        TextBox3.Text = r
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        x = Val(TextBox1.Text)
+        y = Val(TextBox2.Text)
+        r = x / y
+        TextBox3.Text = r
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        x = Val(TextBox1.Text)
+        y = Val(TextBox2.Text)
+        r = x + y
+        TextBox3.Text = r
+    End Sub
+End Class
